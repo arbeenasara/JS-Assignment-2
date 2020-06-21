8770b4122e3b3e2b845f207512c488b7eea3dbac
arbeenasara <arbeena27memon@gmail.com>
2020-06-21 16:58:20 +0500

    complete

---
+++ app.js
@@ -0,0 +1,75 @@
+document.getElementById("demo").innerHTML = "<h1>Chapter 21-25</h1>";
+
+document.getElementById("q1").innerHTML = "<h3>Question # 1</h3>";
+// var firstName = prompt("Enter Your First Name");
+// var lastName = prompt("Enter Your Last Name");
+// var fullName = firstName + " " + lastName;
+// alert("Hello! " + fullName + " How are you?");
+
+document.getElementById("q2").innerHTML = "<h3>Question # 2</h3>";
+// var mob = prompt("Enter your favourite Mobile Phone Model");
+// document.write("My favourite phone is: " + mob + "<br>" + "Length of String: " + mob.length);
+
+document.getElementById("q3").innerHTML = "<h3>Question # 3</h3>";
+// var str = "Pakistani";
+// var ind = str.indexOf("n");
+// document.write("String: " + str + "<br>" + "Index of 'n': " + ind);
+
+document.getElementById("q4").innerHTML = "<h3>Question # 4</h3>";
+// var str = "Hello World";
+// var ind = str.lastIndexOf("l");
+// document.write("String: " + str + "<br>" + "Last Index of 'l': " + ind);
+
+document.getElementById("q5").innerHTML = "<h3>Question # 5</h3>";
+// var str = "Pakistani";
+// var ind = str.slice(3,4);
+// document.write("String: " + str + "<br>" + "Character at index 3: " + ind); 
+
+document.getElementById("q6").innerHTML = "<h3>Question # 6</h3>";
+// var firstName = prompt("Enter Your First Name");
+// var lastName = prompt("Enter Your Last Name");
+// var fullName = firstName.concat(" ",lastName);
+// alert("Hello! " + fullName + " How are you?");
+
+document.getElementById("q7").innerHTML = "<h3>Question # 7</h3>";
+// var str = "Hyderabad"; 
+// var txt = str.replace("Hyder","Islam");
+// document.write("City: " + str + "<br>" + "After replacement: " + txt);
+
+document.getElementById("q8").innerHTML = "<h3>Question # 8</h3>";
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var txt = message.replace(/and/g,"&");
+// document.write(txt);
+
+document.getElementById("q9").innerHTML = "<h3>Question # 9</h3>";
+// var i = "472";
+// var a = 472;
+// document.write("Value: " + i + "<br>" + "Type: " + typeof i + "<br>" + "Value: " + a + "<br>" + "Type: " + typeof a);
+
+document.getElementById("q10").innerHTML = "<h3>Question # 10</h3>";
+// var text = prompt("Write something in Lowercase");
+// document.write("User input: " + text + "<br>" + "Uppercase: " + text.toUpperCase());
+
+document.getElementById("q11").innerHTML = "<h3>Question # 11</h3>";
+// function sentenceCase (str) {
+//     if ((str===null) || (str===''))
+//          return false;
+//     else
+//      str = str.toString();
+  
+//    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+//   }
+  
+// document.write(sentenceCase("Title Case: " + prompt('Write something in Lowercase')));
+
+document.getElementById("q12").innerHTML = "<h3>Question # 13</h3>";
+
+
+
+
+
+
+
+
+
+
